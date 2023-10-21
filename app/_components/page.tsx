286b74effc584434a0ec243393d772e89a97c501
ankush <ankush>
2023-10-21 19:14:11 +0530

    understanding of different routes

---
+++ app/_components/page.tsx
@@ -0,0 +1,5 @@
+const Components = () => {
+  return <h1>You can prevant folder by using "_".</h1>;
+};
+
+export default Components;
---
+++ app/regular/page.tsx
@@ -0,0 +1,5 @@
+const Regular = () => {
+  return <h1>This is a reguler type routes</h1>;
+};
+
+export default Regular;
