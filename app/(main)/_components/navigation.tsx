c3358b5e74bdd11c2c5544680ddd8250901466c1
ankush <ankush>
2023-10-26 23:04:53 +0530

    Document list in sidebar

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -4,18 +4,18 @@ import { ChevronsLeft, MenuIcon, PlusCircle, Search, Settings } from "lucide-rea
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
-import { useMutation, useQuery } from "convex/react";
+import { useMutation } from "convex/react";
 
 import { cn } from "@/lib/utils";
 import { UserItem } from "./user-item";
 import { api } from "@/convex/_generated/api";
 import { toast } from "sonner";
 import { Item } from "./item";
+import { DocumentList } from "./document-list";
 
 export const Navigation = () => {
   const pathname = usePathname();
-  const isMobile = useMediaQuery("(max-width: 768px)");
-  const documents = useQuery(api.documents.get);
+  const isMobile = useMediaQuery("(max-width: 768px)");  
   const create = useMutation(api.documents.create);
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<"aside">>(null);
@@ -111,9 +111,7 @@ export const Navigation = () => {
           <Item onClick={handleCreate} label="New page" icon={PlusCircle} />
         </div>
         <div className="mt-4">
-          {documents?.map((document) => (
-            <p>{document.title}</p>
-          ))}
+          <DocumentList/>
         </div>
         <div onMouseDown={handleMouseDown} onClick={resetWidth} className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" />
       </aside>
