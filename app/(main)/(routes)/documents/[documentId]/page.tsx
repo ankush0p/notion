26ee60fe1279bddd6e5db4f83dac8d4f548ae2eb
ankush <ankush>
2023-11-05 13:34:28 +0530

    added Toolbar component (Icon, Title)

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,7 +1,49 @@
 "use client";
 
-const DocumentsPage = () => {
-  return ( <h1 className="m-10">Welcome to documents pages.</h1> );
+import { useMutation, useQuery } from "convex/react";
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
+
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { Toolbar } from "@/components/toolbar";
+import { Skeleton } from "@/components/ui/skeleton";
+
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<"documents">;
+  };
 }
- 
-export default DocumentsPage;
+
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+  const document = useQuery(api.documents.getById, { documentId: params.documentId });
+  const update = useMutation(api.documents.update);
+
+  const onChange = (content: string) => {
+    update({ id: params.documentId, content });
+  };
+
+  if (document === undefined) {
+    return (
+      <div>
+        <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
+          <div className="space-y-4 pl-8 pt-4"></div>
+        </div>
+      </div>
+    );
+  }
+
+  if (document === null) {
+    return <div>Not found</div>;
+  }
+
+  return (
+    <div className="pb-40">
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+      </div>
+    </div>
+  );
+};
+
+export default DocumentIdPage;
